f6f1f90f8627d141a7738f606f25371d94f17140
thossa01 <145376774+thossa01@users.noreply.github.com>
2024-12-21 20:18:01 -0500

    Update generalBoardRules.java

--- com/Chess3D/core/board/generalBoardRules.java
+++ com/Chess3D/core/board/generalBoardRules.java
@@ -1,5 +1,9 @@
 package com.Chess3D.core.board;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 public class generalBoardRules {
     
 
@@ -16,8 +20,8 @@ public class generalBoardRules {
     public static final boolean [] seventhRow = rowTiles(48);
     public static final boolean [] eighthRow = rowTiles(56);
     
-    //public static final String[] ALGEBREIC_NOTATION = initializeAlgebricNotation();
-    //public static final Map<String, Integer> POSITIONAL_NOTATION = initializePositionalNotation();
+    public static final String[] ALGEBREIC_NOTATION = initializeAlgebricNotation();
+    public static final Map<String, Integer> POSITIONAL_NOTATION = initializePositionalNotation();
 
     private static boolean[] columnTiles(int columnFirstTile){
 
@@ -45,11 +49,32 @@ public class generalBoardRules {
     public static boolean isValidTileCoordinate(final int possibleDestination) {
         return possibleDestination >= 0 && possibleDestination < 64;
     }
+
+    private static String[] initializeAlgebricNotation() {
+        return new String[] {
+                "a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8",
+                "a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7",
+                "a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6",
+                "a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5",
+                "a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4",
+                "a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3",
+                "a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2",
+                "a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1"
+        };
+    }
+
+    private static Map<String, Integer> initializePositionalNotation() {
+        final Map<String, Integer> positionalNotation = new HashMap<>();
+        for (int i = 0; i < 64; i++) {
+            positionalNotation.put(ALGEBREIC_NOTATION[i], i);
+        }
+        return Collections.unmodifiableMap(positionalNotation);
+    }
     
-    /*public static int getCoordinateAtPosition(final String position) {
-        return POSITIONAL_NOTATION.getOrDefault(position, -1);
+    public static int getCoordinateAtPosition(final String position) {
+        return POSITIONAL_NOTATION.get(position);
     }
     public static String getPositionAtCoordinate(final int coordinate) {
-
-    }*/
+        return ALGEBREIC_NOTATION[coordinate];
+    }
 }
